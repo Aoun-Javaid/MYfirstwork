baf1a88f36407b08335dea4a64feeb9a515ff9d2
aounj <aounjavaid01@gmail.comm>
2023-01-11 03:55:30 +0400

    markets section

--- src/app/client/pages/dashboard/markets/markets.component.ts
+++ src/app/client/pages/dashboard/markets/markets.component.ts
@@ -9,16 +9,35 @@ export class MarketsComponent implements OnInit {
 
   constructor() { }
   catTabs=[
-    {id:1,cat_name:'cricket',cat_icon:'assets/icons/casino'},
-    {id:2,cat_name:'football',cat_icon:'assets/icons/casino'},
-    {id:3,cat_name:'tennis',cat_icon:'assets/icons/casino'},
-    {id:4,cat_name:'casino',cat_icon:'assets/icons/casino'},
-    {id:5,cat_name:'election',cat_icon:'assets/icons/casino'},
-    {id:1,cat_name:'cricket',cat_icon:'assets/icons/casino'},
-    {id:2,cat_name:'football',cat_icon:'assets/icons/casino'},
-    {id:3,cat_name:'tennis',cat_icon:'assets/icons/casino'},
-    {id:4,cat_name:'casino',cat_icon:'assets/icons/casino'},
-    {id:5,cat_name:'election',cat_icon:'assets/icons/casino'},
+    {id:1,cat_name:'cricket',cat_icon:'assets/icons/casino.png'},
+    {id:2,cat_name:'football',cat_icon:'assets/icons/football.png'},
+    {id:3,cat_name:'tennis',cat_icon:'assets/icons/casino.png'},
+    {id:4,cat_name:'casino',cat_icon:'assets/icons/casino.png'},
+    {id:5,cat_name:'election',cat_icon:'assets/icons/casino.png'},
+  ];
+  markets=[
+    {
+      m_name:'Brisbane Heat v Perth Scorchers',m_subtitle:"Twenty20 Big Bash"
+    },
+    {
+      m_name:'Brisbane Heat v Perth Scorchers',m_subtitle:"Twenty20 Big Bash"
+    },
+    {
+      m_name:'Brisbane Heat v Perth Scorchers',m_subtitle:"Twenty20 Big Bash"
+    },
+    {
+      m_name:'Brisbane Heat v Perth Scorchers',m_subtitle:"Twenty20 Big Bash"
+    },
+    {
+      m_name:'Brisbane Heat v Perth Scorchers',m_subtitle:"Twenty20 Big Bash"
+    },
+    {
+      m_name:'Brisbane Heat v Perth Scorchers',m_subtitle:"Twenty20 Big Bash"
+    },
+    {
+      m_name:'Brisbane Heat v Perth Scorchers',m_subtitle:"Twenty20 Big Bash"
+    },
+
   ]
   ngOnInit(): void {
   }
