ed82a457d41fca3bedbfb942c49569a48677640a
aounj <aounjavaid01@gmail.comm>
2023-01-10 21:28:53 +0400

    components added

--- src/app/client/client-routing.module.ts
+++ src/app/client/client-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ClientComponent} from "./client.component";
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: ClientComponent, children: [
+      {path: '', component: DashboardComponent},
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClientRoutingModule } from './client-routing.module';
+import { ClientComponent } from './client.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { FooterComponent } from './partials/footer/footer.component';
+import { BottomNavComponent } from './partials/bottom-nav/bottom-nav.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ClientComponent,
+    HeaderComponent,
+    FooterComponent,
+    BottomNavComponent,
+    DashboardComponent
+  ],
   imports: [
     CommonModule,
     ClientRoutingModule
