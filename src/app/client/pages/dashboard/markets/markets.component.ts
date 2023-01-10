4defc64b500f2a0119c8fe17f1d81dc30a11cbc1
aounj <aounjavaid01@gmail.comm>
2023-01-11 01:19:01 +0400

    ratesss

--- src/app/client/pages/dashboard/markets/markets.component.ts
+++ src/app/client/pages/dashboard/markets/markets.component.ts
@@ -8,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class MarketsComponent implements OnInit {
 
   constructor() { }
-
+  catTabs=[
+    {id:1,cat_name:'cricket',cat_icon:'assets/icons/casino'},
+    {id:2,cat_name:'football',cat_icon:'assets/icons/casino'},
+    {id:3,cat_name:'tennis',cat_icon:'assets/icons/casino'},
+    {id:4,cat_name:'casino',cat_icon:'assets/icons/casino'},
+    {id:5,cat_name:'election',cat_icon:'assets/icons/casino'},
+    {id:1,cat_name:'cricket',cat_icon:'assets/icons/casino'},
+    {id:2,cat_name:'football',cat_icon:'assets/icons/casino'},
+    {id:3,cat_name:'tennis',cat_icon:'assets/icons/casino'},
+    {id:4,cat_name:'casino',cat_icon:'assets/icons/casino'},
+    {id:5,cat_name:'election',cat_icon:'assets/icons/casino'},
+  ]
   ngOnInit(): void {
   }
 
