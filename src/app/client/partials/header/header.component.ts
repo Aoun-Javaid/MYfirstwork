1ff7802551a22c04d9dbca798b1c0d7b9aa555f5
fahadkhan1229 <fahadkhan1229@gmail.com>
2023-01-11 16:05:36 +0400

    side nav created

--- src/app/client/partials/header/header.component.ts
+++ src/app/client/partials/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  isSideBarOpen = false;
 
-  constructor() { }
+  onClickedOutside(event: any) {
+    if (event.target?.id !== 'toggle-sidemenu') {
+      this.isSideBarOpen = false;
+      document.body.className = document.body.className.replace(/left-bar-enabled/g, '').trim();
+    }
+  }
+
+  sidenavOpen() {
+    /*$('<div class="modal-backdrop fade show"></div>').appendTo('body');
+    $("#" + id).addClass('show');
+    $("#" + id).css('display', 'block');*/
+    //document.getElementsByTagName('element')[0].classList.remove('d-none');
+
+    // this.isSideBarOpen = !this.isSideBarOpen;
+    if (!this.isSideBarOpen) {
+      document.body.className += ' left-bar-enabled';
+      this.isSideBarOpen = true;
+    } else {
+      document.body.className = document.body.className.replace(/left-bar-enabled/g, '').trim();
+      this.isSideBarOpen = false;
+    }
+  }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
