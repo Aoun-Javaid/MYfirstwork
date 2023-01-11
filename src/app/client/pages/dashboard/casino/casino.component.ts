9f9920b89d987924c8c380d69fbad3b26cc0a1c0
Aoun-Javaid <78606381+Aoun-Javaid@users.noreply.github.com>
2023-01-11 11:13:09 +0400

    casino

--- src/app/client/pages/dashboard/casino/casino.component.ts
+++ src/app/client/pages/dashboard/casino/casino.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CasinoComponent implements OnInit {
 
+
+  casinoCards =[
+    { imglink:"assets/casino/1.jpeg",alternate:""},
+    { imglink:"assets/casino/2.jpeg",alternate:""},
+    { imglink:"assets/casino/1.jpeg",alternate:""},
+    { imglink:"assets/casino/2.jpeg",alternate:""},
+    { imglink:"assets/casino/1.jpeg",alternate:""},
+    { imglink:"assets/casino/2.jpeg",alternate:""},
+  ]
+  listCasino=[
+    { name:"Popular",index:1},
+    { name:"Teen patti",index:2},
+    { name:"lucky7",index:3},
+    { name:"Dargon Tiger",index:4},
+    { name:"Andar bahar",index:5},
+    { name:"Baccarat",index:6},
+    { name:"Dargon Tiger",index:7},
+    { name:"Andar bahar",index:8},
+    { name:"Baccarat",index:9}
+  ]
+
+  isActive : boolean =true;
+  step:number=1;
+  cliclTab(stepValue:number){
+    this.step = stepValue;
+    // localStorage.setItem('step', JSON.stringify(this.step));
+  }
   constructor() { }
 
   ngOnInit(): void {
