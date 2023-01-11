d64bb92b354b007383326c4e5ebeceeb784e629b
aounj <aounjavaid01@gmail.comm>
2023-01-11 20:06:10 +0400

    some changings

--- src/app/client/partials/header/header.component.ts
+++ src/app/client/partials/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, EventEmitter, HostListener, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,21 +7,23 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   isSideBarOpen = false;
+  @Output() clickOutside = new EventEmitter<void>();
 
-  onClickedOutside(event: any) {
-    if (event.target?.id !== 'toggle-sidemenu') {
-      this.isSideBarOpen = false;
-      document.body.className = document.body.className.replace(/left-bar-enabled/g, '').trim();
+  constructor(private elementRef: ElementRef) {
+  }
+  ngOnInit(): void {
+  }
+  @HostListener('document:click', ['$event.target'])
+  public onClick(target: any) {
+    document.body.className = document.body.className.replace(/left-bar-enabled/g, '').trim();
+    const clickedInside = this.elementRef.nativeElement.contains(target);
+    if (!clickedInside) {
+      this.isSideBarOpen = false
+      this.clickOutside.emit();
     }
   }
 
   sidenavOpen() {
-    /*$('<div class="modal-backdrop fade show"></div>').appendTo('body');
-    $("#" + id).addClass('show');
-    $("#" + id).css('display', 'block');*/
-    //document.getElementsByTagName('element')[0].classList.remove('d-none');
-
-    // this.isSideBarOpen = !this.isSideBarOpen;
     if (!this.isSideBarOpen) {
       document.body.className += ' left-bar-enabled';
       this.isSideBarOpen = true;
@@ -29,12 +31,6 @@ export class HeaderComponent implements OnInit {
       document.body.className = document.body.className.replace(/left-bar-enabled/g, '').trim();
       this.isSideBarOpen = false;
     }
-  }
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
   }
 
 }
