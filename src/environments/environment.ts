ab5b2a9b6c303699a7f42b996d2fecf79e8359b6
fahadkhan1229 <fahadkhan1229@gmail.com>
2023-01-12 21:47:24 +0400

    env added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://130.172.2.149:4444',
   apiUrl: 'http://130.172.2.149:4444/api',
-  apiV1: 'http://130.172.1.139:4567'
+  apiV1: 'http://130.172.1.139:4567/v1'
 };
 
 /*
