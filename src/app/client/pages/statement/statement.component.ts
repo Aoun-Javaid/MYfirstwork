4cfaecce58f3177e315d253cb54d86a3742d9623
aounj <aounjavaid01@gmail.comm>
2023-01-14 17:26:07 +0400

    statemnet

--- src/app/client/pages/statement/statement.component.ts
+++ src/app/client/pages/statement/statement.component.ts
@@ -14,6 +14,33 @@ export class StatementComponent implements OnInit {
   dtOptions: DataTables.Settings = {};
 
   ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength:1,
+      tabIndex:2,
+      ajax: 'data/data.json',
+      columns: [{
+        title: 'Date/Time',
+        data: 'Date/Time'
+      }, {
+        title: 'Deposit',
+        data: 'Deposit'
+      },
+        {
+          title: 'Withdraw',
+          data: 'Withdraw'
+        },
+        {
+        title: 'Balance',
+        data: 'Balance'
+      }, {
+        title: 'Remark',
+        data: 'Remark'
+      }, {
+        title: 'From/To',
+        data: 'From/To'
+      }]
 
+    };
   }
 }
