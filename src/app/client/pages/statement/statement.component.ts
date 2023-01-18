911412b419b89e27b1649fdc387a4f4efa96b6c4
aounj <aounjavaid01@gmail.comm>
2023-01-18 11:24:04 +0400

    statement date added

--- src/app/client/pages/statement/statement.component.ts
+++ src/app/client/pages/statement/statement.component.ts
@@ -107,8 +107,10 @@ export class StatementComponent implements OnInit {
 
   dtTrigger: Subject<any> = new Subject<any>()
   accountStatement: any = [];
-
+  startDate: string;
+  endDate: string;
   ngOnInit(): void {
+    this.updateDate();
     this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 1,
@@ -155,8 +157,71 @@ export class StatementComponent implements OnInit {
 // ngOnDestroy(): void {
 //    this.dtTrigger.unsubscribe();
 // }
+  completeDate: Date;
+  localCompleteDate: string;
+  ngModelStartChange($event: any) {
+    this.startDate = $event.target.value;
+    //debugger
+  }
+  ngModelEndChange($event: any) {
+    this.endDate = $event.target.value;
+
+  }
+  updateDate() {
+
+    let sDate: any = new Date();
+    sDate = sDate.toString();
+    let date = sDate.substr(8, 2);
+    //this.startDate = new Date();
+    this.completeDate = new Date();
+    let hours: any = this.completeDate.getHours();
+    if (hours < 10) {
+      hours = '0' + hours;
+    }
+    let minutes: any = this.completeDate.getMinutes();
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+    let hrstest = date.split("");
+    if (Number(hrstest[0]) != 0 && Number(date) < 10) {
+      date = '0' + date;
+    }
 
 
+    this.completeDate.setDate(this.completeDate.getDate() - 1);
+    this.localCompleteDate = this.completeDate.toISOString();
+    this.startDate = this.completeDate.toISOString()
+    this.startDate = this.startDate.substr(0, 10);
+    this.startDate = this.startDate + 'T' + hours + ':' + minutes;
+    // 2022-04-20T16:23
+
+    // this.startDate = this.localCompleteDate.substring(0, this.localCompleteDate.length - 1);
+    // this.startDate=new Date(this.completeDate.getTime() - (this.completeDate.getTimezoneOffset() * 60000)).toISOString().replace(/T/, ' ').replace(/\..+/, ''));
+    //   this.startDate = this.startDate.substr(0, 16);
+    //this.startDate.setDate(this.startDate.getDate() - 4);
+    let min = Number(minutes);
+    if(min<9){
+      minutes= '0'+(min+1);
+    }
+    else if(min==59){
+      minutes='00';
+      hours=(Number(hours)+1).toString();
+    }
+    else{
+      minutes=(min+1).toString();
+    }
+    var dates = new Date();
+    let month:any = dates.getMonth()+1;
+
+    if(Number(month)<10){
+      month='0'+month;
+    }
+
+    this.endDate = new Date().toISOString();
+    this.endDate = this.endDate.substr(0, 4);
+
+    this.endDate = this.endDate+'-'+month+'-'+ date + 'T' + hours + ':' + minutes;
+  }
   SubmitdataTable() {
     this.draw.startDate = this.statementForm.value.startDate;
     this.draw.endDate = this.statementForm.value.endDate;
